255be90bc479a2b74ed71b2c68dc5a80125f151f
Zilfs <rheinlandcast@gmail.com>
2026-08-06 03:27:22 +0700

    feat: added soft delete to product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'sku', 'selling_price', 'cost', 'stock_quantity', 'unit', 'is_active'])]
 class Product extends Model
 {
-    //
+    use SoftDeletes;
 }

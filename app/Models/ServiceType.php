b048b981f96f8b810db6c652ba79a95c495055c0
Zilfs <rheinlandcast@gmail.com>
2026-08-06 02:23:19 +0700

    feat: add service type model

---
+++ app/Models/ServiceType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['name', 'default_price', 'default_commission_price', 'is_active'])]
+class ServiceType extends Model
+{
+    //
+}
